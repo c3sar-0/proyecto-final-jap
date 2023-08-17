f65b770bd2dac2ce69edc3bb70f2a6c0cbd3eec3
patriciocarbonell <carbonellpato@gmail.com>
2023-08-17 16:05:36 -0300

    register

---
+++ js/register.js
@@ -0,0 +1,8 @@
+const formulario = document.getElementById("formulario")
+
+formulario.addEventListener("submit", function(event){
+    
+    event.preventDefault()
+
+    window.location.href="index.html"
+})
