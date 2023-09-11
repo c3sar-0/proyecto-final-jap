0e2dadc4c9dd6d9dbc3c6555b8099887bd136e81
ezequielvera11 <ezevc11@gmail.com>
2023-09-11 15:37:10 -0300

    Corrección de errores

--- js/product-info.js
+++ js/product-info.js
@@ -14,24 +14,49 @@ document.addEventListener("DOMContentLoaded",()=>
             divInfo.innerHTML += 
             `
                 <div>
-                    <h1>${infoObj.data.name}</h1>
-                    <br>
-                    <h4>Precio</h4>
+                    <h3 style="margin-top: 1.3rem;">${infoObj.data.name}</h3>
+                    <hr style="margin-top: 1.8rem;">
+                    <strong>Precio</strong>
                     <p>${infoObj.data.currency}${infoObj.data.cost}</p>
-                    <h4>Descripcion</h4>
+                    <strong>Descripción</strong>
                     <p>${infoObj.data.description}</p>
-                    <h4>Categoria</h4>
+                    <strong>Categoría</strong>
                     <p>${infoObj.data.category}</p>
-                    <h4>Cantidad de vendidos</h4>
+                    <strong>Cantidad de vendidos</strong>
                     <p>${infoObj.data.soldCount}</p>
-                    <h4>Imagenes ilustrativas</h4>   
+                    <strong>Imágenes ilustrativas</strong>   
                 </div>    
             `;
             infoObj.data.images.forEach(img => 
             {
-                    divInfo.innerHTML += `<img src= "${img}">`
-            });
+                divInfo.innerHTML += `<img src= "${img}" style='width: 15rem; margin-top: 1.3rem;'>`
+            });    
         }
         console.log(infoObj);
+    });
+    
+    //(E3)Accedemos al json de los comentarios
+    //(E3)Con un forEach recorremos el array para poder mostrar los comentarios y puntajes de cada uno de los productos
+    const urlComments = `https://japceibal.github.io/emercado-api/products_comments/${productInfo}.json`;
+    getJSONData(urlComments)
+    .then(infoObj=> {
+        if(infoObj.status === "ok"){
+            divInfo.innerHTML += `
+            <h4 style="margin-top: 3rem;">Comentarios</h4>`
+            infoObj.data.forEach(element => {
+                divInfo.innerHTML += `
+                <div>
+                    <br>
+                    <p>${element.user} - ${element.dateTime} - 
+                        <span class="fa fa-star ${element.score >= 1 && "checked"}"></span>
+                        <span class="fa fa-star ${element.score >= 2 && "checked"}"></span>
+                        <span class="fa fa-star ${element.score >= 3 && "checked"}"></span>
+                        <span class="fa fa-star ${element.score >= 4 && "checked"}"></span>
+                        <span class="fa fa-star ${element.score == 5 && "checked"}"></span> </p>
+                    <p>${element.description}</p>
+                </div>    
+            `;
+            });   
+        }
     });
 })
