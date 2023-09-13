1299b43b69965f621421bcfef895f3a8af5192fb
Vale <valentinanaviacabrera@gmail.com>
2023-09-13 18:26:16 -0300

    desafíate + documentación de código

--- js/product-info.js
+++ js/product-info.js
@@ -1,10 +1,15 @@
-//id de div linea 40
-const divInfo = document.getElementById("product-info")
+document.addEventListener("DOMContentLoaded",()=>{
+    //id de div linea 40
+    /*(E3) Creamos constantes donde almacenamos la información que creamos en el html, 
+    además guardamos la url con el producto y los llamamos según el id.*/ 
 
-document.addEventListener("DOMContentLoaded",()=>
-{
+    const divInfo = document.getElementById("product-info")
     const productInfo = localStorage.getItem('idProduc');
+    let puntuacion = 0;
+    const stars = document.querySelectorAll(".star");
     const urlInfo = `https://japceibal.github.io/emercado-api/products/${productInfo}.json`
+
+    /* (E3) con el JSONData accedemos ala información de cada producto y creamos el cuerpo del html*/
     getJSONData(urlInfo)
     .then((infoObj)=>
     {
@@ -29,7 +34,7 @@ document.addEventListener("DOMContentLoaded",()=>
             `;
             infoObj.data.images.forEach(img => 
             {
-                divInfo.innerHTML += `<img src= "${img}" id="imgProductos">`
+                divInfo.innerHTML += `<img src= "${img}" class="imgProductos">`
             });    
         }
         console.log(infoObj);
@@ -47,7 +52,7 @@ document.addEventListener("DOMContentLoaded",()=>
             <h4 id="tituloComentarios">Comentarios</h4>`
             infoObj.data.forEach(element => {
                 divInfo.innerHTML += `
-                <div id="comentario">
+                <div class="comentario">
                     <br>
                     <p><strong> ${element.user} </strong> - ${element.dateTime} - 
                         <span class="fa fa-star ${element.score >= 1 && "checked"}"></span>
@@ -61,17 +66,39 @@ document.addEventListener("DOMContentLoaded",()=>
             });   
         }
     });
-})
+    
+    /*(E3) cuando el usuario envía la información, pasa a mostrarse en pantalla junto al resto de las opiniones*/
+    document.getElementById("formPuntuacion").addEventListener("submit", (e) =>{
+        e.preventDefault();
+        divInfo.innerHTML += `
+        <div class="comentario">
+        <br>
+        <p><strong> ${localStorage.getItem("correo")} </strong> - ${new Date().toLocaleDateString()} - 
+            <span class="fa fa-star ${puntuacion >= 1 && "checked"}"></span>
+            <span class="fa fa-star ${puntuacion >= 2 && "checked"}"></span>
+            <span class="fa fa-star ${puntuacion >= 3 && "checked"}"></span>
+            <span class="fa fa-star ${puntuacion >= 4 && "checked"}"></span>
+            <span class="fa fa-star ${puntuacion == 5 && "checked"}"></span> </p>
+        <p>${document.getElementById("opinionUsuario").value}</p>
+    </div> `
 
-const stars = document.querySelectorAll(".star");
+    //(E3) una vez que se manda la info, se limpia el valor del textarea y la calificación por estrellas
+    document.getElementById("opinionUsuario").value = "";
+    stars.forEach((star)=>{
+        star.classList.remove("checked");
+    });   
+    });
 
-stars.forEach((star, index) => { 
-    star.addEventListener("click", function() {
-        for (let i= 0; i<=index; i++)  {
-            stars[i].classList.add("checked");
-        }
-        for (let i=index+1; i<stars.length; i++) {
-            stars[i].classList.remove("checked");
-        }
-    })
-})
+    //(E3) Creamos funcion que pinte estrellas segun la calificacion del usuario 
+    stars.forEach((star, index) => { 
+        star.addEventListener("click", function() {
+            for (let i= 0; i<=index; i++)  {
+                stars[i].classList.add("checked");
+                puntuacion = i+1;
+            }
+            for (let i=index+1; i<stars.length; i++) {
+                stars[i].classList.remove("checked");
+            }
+        });
+    });
+});
