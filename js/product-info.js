9867f965f2864e2c44750f03b21b9882c23ce264
domibautista <domibauti1234@gmail.com>
2023-09-13 17:04:52 -0300

    Puntuación estrellas

--- js/product-info.js
+++ js/product-info.js
@@ -63,3 +63,15 @@ document.addEventListener("DOMContentLoaded",()=>
     });
 })
 
+const stars = document.querySelectorAll(".star");
+
+stars.forEach((star, index) => { 
+    star.addEventListener("click", function() {
+        for (let i= 0; i<=index; i++)  {
+            stars[i].classList.add("checked");
+        }
+        for (let i=index+1; i<stars.length; i++) {
+            stars[i].classList.remove("checked");
+        }
+    })
+})
