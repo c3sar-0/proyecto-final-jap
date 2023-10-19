78f849bc6c176efd66a35538f2793dd14dae7f67
facundoarellano <107625695+facundoarellanojap@users.noreply.github.com>
2023-10-19 14:14:39 -0300

    reload

--- js/cart.js
+++ js/cart.js
@@ -20,6 +20,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   //const estructura = (compra) =>
   //{
   /*(E5)se crea una estructura prototipo con id para usarlos en el evento "input"*/
+  let numProduct = 0
   products.forEach((articulo) => {
     const input = document.createElement("input");
     input.classList.add("cantidadArticulos");
@@ -33,15 +34,26 @@ document.addEventListener("DOMContentLoaded", async () => {
         articulo.currency + " " + articulo.unitCost * e.target.value;
     });
 
-    const row = document.createElement("tr");
+    const row = document.createElement("tr", {id: "Art"+articulo.id});
     row.innerHTML += `
-        <th class="d-lg-block d-md-none d-sm-none d-none"><img class="cart-img" src="${articulo.image}"></th>
-        <th>${articulo.name}</th>
-        <th class="d-lg-table-cell d-md-none d-sm-none d-none">${articulo.currency} ${articulo.unitCost}</th>
-        <th></th>
-        <th id="subTotal-${articulo.id}">${articulo.currency} ${articulo.unitCost}</th>
+        <td class="d-lg-block d-md-none d-sm-none d-none"><img class="cart-img" src="${articulo.image}"></td>
+        <td>${articulo.name}</td>
+        <td class="d-lg-table-cell d-md-none d-sm-none d-none">${articulo.currency} ${articulo.unitCost}</td>
+        <td></td>
+        <td id="subTotal-${articulo.id}">${articulo.currency} ${articulo.unitCost}</td>
+        <td><input type="button" id="${numProduct}" onclick="eliminarArt(id)"></td>
       `;
     container.appendChild(row);
-    row.querySelector("th:nth-child(4)").appendChild(input);
+    row.querySelector("td:nth-child(4)").appendChild(input);
+    numProduct++;
   });
 });
+
+function eliminarArt(id){
+
+  let nuevaArray= JSON.parse(localStorage.getItem("carrito"));
+  nuevaArray.splice(id,1)
+
+  localStorage.setItem('carrito', JSON.stringify(nuevaArray))
+  location.reload();
+}
