fc2f09178fe784bee65c815dc39543f33f0f3237
96Diego <96DiegoTorres@gmail.com>
2023-10-19 04:26:46 -0300

    Modificaciones para el punto 1

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,15 @@
+//(E6)
+const precioSubTotal = document.getElementById("sub");
+const precioEnvio = document.getElementById("costEnvio");
+const precioTotal = document.getElementById("pagoTotal");
+const radios = document.getElementById("radios");
+const infoCostos = document.getElementById("infoCostos");
+//(E6)lista con identificador y precio/ variabes para subtotal, envio y total
+let subProductos=[];
+let subTotal=0
+let envio=0;
+let total=0;
+
 document.addEventListener("DOMContentLoaded", async () => {
   const container = document.getElementById("products-container");
   const url = "https://japceibal.github.io/emercado-api/user_cart/25801.json";
@@ -14,11 +26,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   } else {
     products = carrito;
   }
-  // console.log(lista);
-  //(E5)en el comentario de abajo creo una posible estructura dandole un parametro, pero por ahora queda anulada, igualmente seria descomentar y
-  //cambiar  en el forEach"datosCompra" por "compra"
-  //const estructura = (compra) =>
-  //{
+
   /*(E5)se crea una estructura prototipo con id para usarlos en el evento "input"*/
   products.forEach((articulo) => {
     const input = document.createElement("input");
@@ -31,6 +39,14 @@ document.addEventListener("DOMContentLoaded", async () => {
       const subTotalElem = document.getElementById(`subTotal-${articulo.id}`);
       subTotalElem.innerHTML =
         articulo.currency + " " + articulo.unitCost * e.target.value;
+        //(E6) cuando se detecta un cambio en el imput agregamos ese cambio al precio del producto en la lista
+        subProductos.forEach(producto=>
+          {
+            if(articulo.id==producto.id)
+            {
+              producto.precio=articulo.unitCost * e.target.value;
+            }
+          })   
     });
 
     const row = document.createElement("tr");
@@ -43,5 +59,45 @@ document.addEventListener("DOMContentLoaded", async () => {
       `;
     container.appendChild(row);
     row.querySelector("th:nth-child(4)").appendChild(input);
+    //(E6) vamos agregando id y precio a medida que estructuramos los productos en el html
+    subProductos.push({id:articulo.id,precio:articulo.unitCost})
   });
+//(E6) calculamos el sub total y lo mostramos en su apartado
+subProductos.forEach(producto=>
+  {
+    subTotal=subTotal+producto.precio
+  })
+precioSubTotal.innerHTML=subTotal
+//(E6) detectamos cualquier cambio dentro de la etiqueta container y modificamos el subTotal en consecuencia
+container.addEventListener("change",()=>
+{
+  subTotal=0
+  subProductos.forEach(producto=>
+    {
+      subTotal=subTotal+producto.precio
+    })
+    precioSubTotal.innerHTML=subTotal
+    if(envio!=0)
+    {
+    total=envio+subTotal;
+    precioTotal.innerHTML=total;
+    }
+})
+
+radios.addEventListener("click",()=>
+{
+  var opciones = document.getElementsByName('envio');
+    for (var radio of opciones)
+    {
+        if (radio.checked) {
+            envio=Math.trunc(subTotal*(radio.value/100));
+        }
+    }
+  precioEnvio.innerHTML=envio
+  total=envio+subTotal;
+  precioTotal.innerHTML=total;
+})
+
 });
+
+
