582443a76eb049bd83d00d384d40c6e7b02603b9
facundoarellano <107625695+facundoarellanojap@users.noreply.github.com>
2023-10-27 12:55:24 -0300

    arreglo, lol

--- js/cart.js
+++ js/cart.js
@@ -98,11 +98,11 @@ document.addEventListener("DOMContentLoaded", async () => {
     //(E6) vamos agregando id y precio a medida que estructuramos los productos en el html
     if(articulo.currency == "UYU")
     {
-      subProductos.push({id:articulo.id,precio:(articulo.unitCost/40)})
+      subProductos.push({id:articulo.id,precio:(articulo.unitCost*articulo.count/40)})
     }
     else
     {
-      subProductos.push({id:articulo.id,precio:articulo.unitCost})
+      subProductos.push({id:articulo.id,precio:articulo.unitCost*articulo.count})
     };
   });
 
